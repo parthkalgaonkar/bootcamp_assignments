d2eb0a2b051c524e79424529525db6dcd49b63a8
Srinivas <srinivaskrn@gmail.com>
2024-06-16 22:02:33 -0500

    Added a moving average design in the sequential examples.

---
+++ sequential/moving_average/moving_average.sv
@@ -0,0 +1,53 @@
+`timescale 1ns/100ps
+module moving_average
+  #(
+    parameter DATA_WD = 16
+    )
+   (
+    input logic                       i_clk,
+    input logic                       i_rstb,
+    input logic signed [DATA_WD-1:0]  i_data,
+    output logic signed [DATA_WD-1:0] o_data
+    );
+
+   /*
+    A 4 tap moving average filter using a delay chain
+    */
+
+   logic signed [DATA_WD-1:0]         r_data_dc [4];
+   logic signed [DATA_WD+1:0]         r_sum;
+   integer                            ii;
+
+   always @(*)
+     begin
+        r_sum = r_data_dc[0] + r_data_dc[1] + r_data_dc[2] + r_data_dc[3];
+     end
+   
+   always @(posedge i_clk or negedge i_rstb)
+     begin
+        if (~i_rstb)
+          begin
+             for (ii = 0; ii < 4; ii++)
+               r_data_dc[ii] <= 'b0;
+             o_data <= 'b0;
+          end
+        else
+          begin
+             r_data_dc[0] <= i_data;
+             for (ii = 1; ii < 4; ii++)
+               r_data_dc[ii] <= r_data_dc[ii-1];
+             o_data <= r_sum[DATA_WD+1:2];
+          end
+     end
+
+`ifdef DUMP_WAVES
+   initial
+     begin
+        $dumpfile("./moving_average.vcd");
+        $dumpvars(0, moving_average);
+     end
+`endif
+   
+endmodule
+
+     
---
+++ sequential/moving_average/test_moving_average.py
@@ -0,0 +1,32 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
+import numpy as np
+
+# Function for generating test vectors
+def gen_test(BW):
+    data_in = np.arange(1000)
+    data_out = np.zeros(1000)
+    for k in range(4, 1000):
+        data_out[k] = np.floor((data_in[k-1] + data_in[k-2] + data_in[k-3] + data_in[k-4])/4)
+    return data_in, data_out
+
+async def reset(dut):
+    dut.i_rstb.value = 1
+    await Timer(1, units = "ns")
+    dut.i_rstb.value = 0
+    await Timer(1, units = "ns")
+    dut.i_rstb.value = 1
+    return
+
+@cocotb.test()
+async def test_adder(dut):
+    [data_in, data_out] = gen_test(dut.DATA_WD.value)
+    dut.i_data.value = 0
+    cocotb.start_soon(Clock(dut.i_clk, 1, units = "ns").start())
+    await reset(dut)
+    for n in range(len(data_in)):
+        await FallingEdge(dut.i_clk)
+        dut.i_data.value = int(data_in[n])
+        if (n > 8):
+            assert dut.o_data.value.signed_integer == data_out[n-1]
